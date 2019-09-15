325ddea6acbf6d4f2baf4e27236d4a21725503a7
ThomasKessell <54597418+TKessell@users.noreply.github.com>
2019-09-15 18:37:09 -0400

    Create Integration.py

---
+++ Integration.py
@@ -0,0 +1,19 @@
+# Domenic Kessell
+# Integration Project 2019
+# Uses every concept learned in COP1500 in one program
+
+name = input("Welcome, what is your name? ")
+print("Hello", name+". This is my python integration project.")
+i = input("To find the average of two numbers, first enter one number: ")
+j = input("Then enter a second number: ")
+i = int(i)
+j = int(j)
+print("The average is the first number + the second number, all divided by two: ", (i+j)/2)
+print("Subtraction: a-b =", i-j)
+print("Addition: a+b =", i+j)
+print("Multiplication: a*b =", i*j)
+print("Division with no remainder: a//b =", i//j)
+print("Powers: a**b: =", i**j)
+print("Modulus division: a%b =", i%j)
+print("All done. Shutting down... -_-")
+
