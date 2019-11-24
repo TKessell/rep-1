7cb502d6e8ef4a0504f5de82a06824497a347664
ThomasKessell <54597418+TKessell@users.noreply.github.com>
2019-11-24 17:59:57 -0500

    Create GameOfLife.java
    Beginning a new personal project for submission 4

---
+++ GameOfLife.java
@@ -0,0 +1,16 @@
+public class GameOfLife
+{
+	public static void main(String[] args)
+	{
+		String[] cells= {"-()-","-()-","-()-"};
+		//System.out.println(cells.length);
+		int gens= 3;
+		for(int i= 0;i < gens;i++)
+    {
+		  System.out.println("Generation " + i + ":");
+		  print(dish);
+		  dish= life(dish);
+    }
+	}		
+}
+ 
