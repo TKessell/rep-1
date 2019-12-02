9fccfa368a498cb5e4b067aa937b5e1416d03b1a
ThomasKessell <54597418+TKessell@users.noreply.github.com>
2019-12-01 23:26:56 -0500

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,67 +1,68 @@
 public class GameOfLife
 {
-	public static void main(String[] args)
-	{
-		String[] cells= {"-()-","-()-","-()-"};
-		//System.out.println(cells.length);
-		int gens= 3;
-		for(int i= 0;i < gens;i++)
-		{
-			System.out.println("Generation " + i + ":");
-			print(dish);
-			dish= life(dish);
-		}
-	}		
+  public static void main(String[] args)
+  {
+    String[] cells= {"-()-","-()-","-()-"};
+    //System.out.println(cells.length);
+    int gens= 3;
+    for(int i= 0;i < gens;i++)
+    {
+      System.out.println("Generation " + i + ":");
+      print(cells);
+      cells= life(cells);
+    }
+  }  
 }
+
 
 public static String[] life(String[] c)
 {
-	String[] newGen= new String[cells.length];
-	for(int row= 0;row < cells.length;row++)
-	{
-		newGen[row]= "";
-		for(int i= 0;i < cells[row].length();i++)
-	{
-	  String above= "";
-	   String same= "";
-			  String below= "";
-	   if(i == 0)
-	  {
-	    above= (row == 0) ? null : cells[row - 1].substring(i,
-							    i + 2);
-	    same= cells[row].substring(i + 1, i + 2);
-	    below= (row == cells.length - 1) ? null : cells[row + 1]
-	     .substring(i, i + 2);
-	      }
-	      else if(i == cells[row].length() - 1)
-	      {
-		above= (row == 0) ? null : cells[row - 1].substring(i - 1,
-								    i + 1);
-	      same= cells[row].substring(i - 1, i);
-		below= (row == cells.length - 1) ? null : cells[row + 1]
-		.substring(i - 1, i + 1);
+  String[] newGen= new String[cells.length];
+  for(int row= 0;row < cells.length;row++)
+  {
+    newGen[row]= "";
+    for(int i= 0;i < cells[row].length();i++)
+    {
+      String above= "";
+      String same= "";
+      String below= "";
+      if(i == 0)
+      {
+        above= (row == 0) ? null : cells[row - 1].substring(i,
+                                                            i + 2);
+        same= cells[row].substring(i + 1, i + 2);
+        below= (row == cells.length - 1) ? null : cells[row + 1]
+          .substring(i, i + 2);
+      }
+      else if(i == cells[row].length() - 1)
+      {
+        above= (row == 0) ? null : cells[row - 1].substring(i - 1,
+                                                            i + 1);
+        same= cells[row].substring(i - 1, i);
+        below= (row == cells.length - 1) ? null : cells[row + 1]
+          .substring(i - 1, i + 1);
       }
       else
-	{
-		above= (row == 0) ? null : cells[row - 1].substring(i - 1,
-								    i + 2);
-	      same= cells[row].substring(i - 1, i)
-	  + cells[row].substring(i + 1, i + 2);
-	below= (row == cells.length - 1) ? null : cells[row + 1]
-	  .substring(i - 1, i + 2);
+      {
+        above= (row == 0) ? null : cells[row - 1].substring(i - 1,
+                                                            i + 2);
+        same= cells[row].substring(i - 1, i)
+          + cells[row].substring(i + 1, i + 2);
+        below= (row == cells.length - 1) ? null : cells[row + 1]
+          .substring(i - 1, i + 2);
       }
       int neighbors= getNeighbors(above, same, below);
       if(neighbors < 2 || neighbors > 3)
       {
-	newGen[row]+= "_";
+        newGen[row]+= "_";
       }
       else if(neighbors == 3)
       {
-	newGen[row]+= "#";
+        newGen[row]+= "#";
       }
       else
       {
-	newGen[row]+= cells[row].charAt(i);
+        newGen[row]+= cells[row].charAt(i);
       }
     }
   }
@@ -93,9 +94,10 @@ public static int getNeighbors(String above, String same, String below)
 }
 
 public static void print(String[] cells)
-{
-	for(String s: cells)
 	{
+	  for(String s: cells)
+	  {
 		System.out.println(s);
+	  }
 	}
 }
